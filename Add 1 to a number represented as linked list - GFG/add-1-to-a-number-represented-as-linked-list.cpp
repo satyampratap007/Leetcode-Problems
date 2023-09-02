4f9212a35b0db9c197a8217ac928dbaf7ad2e23f
Satyam Pratap <satyampratap007@gmail.com>
2023-09-02 15:50:15 +0530

    Added solution - LeetHub

---
+++ Add 1 to a number represented as linked list - GFG/add-1-to-a-number-represented-as-linked-list.cpp
@@ -0,0 +1,114 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data%10; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+// } Driver Code Ends
+//User function template for C++
+
+/* 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+    public:
+    Node* reverse(Node* &head)
+    {
+        Node* prev = NULL;
+        Node* curr = head;
+        
+        while(curr)
+        {
+            Node* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    
+    Node* addOne(Node *head) 
+    {
+        head = reverse(head);
+        int carry = 1;
+        Node*temp = head;
+        Node*prev = NULL;
+        while(temp)
+        {
+            prev = temp;
+            int num = temp->data+carry;
+            temp->data = num%10;
+            carry = num/10;
+            temp = temp->next;
+        }
+        if(carry != 0){
+            Node* car = new Node(carry);
+            prev->next = car;
+            car->next = NULL;
+        }
+        head = reverse(head);
+        return head;
+    }
+    
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        
+        Node* head = new Node( s[0]-'0' );
+        Node* tail = head;
+        for(int i=1; i<s.size(); i++)
+        {
+            tail->next = new Node( s[i]-'0' );
+            tail = tail->next;
+        }
+        Solution ob;
+        head = ob.addOne(head);
+        printList(head); 
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
