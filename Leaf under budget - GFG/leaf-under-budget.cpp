d720a2b5cd42899107f1974b779537d82d1ac851
Satyam Pratap <satyampratap007@gmail.com>
2023-09-02 14:58:52 +0530

    Added solution - LeetHub

--- Leaf under budget - GFG/leaf-under-budget.cpp
+++ Leaf under budget - GFG/leaf-under-budget.cpp
@@ -164,6 +164,7 @@ public:
                         k -= level;
                         count++;
                     }
+                    else    return count;
                 }
                 if(temp->left)  q.push(temp->left);
                 if(temp->right) q.push(temp->right);
