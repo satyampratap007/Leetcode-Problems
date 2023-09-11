59c586f4b9674c0b0e5c6f09aae6b5fe102c0a22
Satyam Pratap <satyampratap007@gmail.com>
2023-09-11 15:18:44 +0530

    Added solution - LeetHub

--- Lucky Numbers - GFG/lucky-numbers.cpp
+++ Lucky Numbers - GFG/lucky-numbers.cpp
@@ -17,7 +17,7 @@ public:
     bool isLucky(int n) {
         for(int i{2};i  <= n ; ++i)
         {
-            if(n >= i &&  n%i == 0)
+            if( n%i == 0)
                 return false;
                 
             n -= (n/i);
