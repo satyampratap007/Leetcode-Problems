9bb98f0094b551ffe33338a8a28883d62edb621f
Satyam Pratap <satyampratap007@gmail.com>
2023-09-21 01:06:23 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -12,31 +12,68 @@ class Solution
     public:
     //Function to find if there is a celebrity in the party or not.
     // Brute Force 
-    int celebrity(vector<vector<int> >& M, int n) 
-    {
-        int pot {-1};
-        for(int i{0}; i < n; ++i)
+    // int celebrity(vector<vector<int> >& M, int n) 
+    // {
+    //     // int pot {-1};
+        // for(int i{0}; i < n; ++i)
+        // {
+        //     bool flag = true;
+        //     for(int j{0}; j < n; ++j)
+        //     {
+        //         if(M[i][j] != 0)
+        //         {
+        //             flag = false;
+        //             break;
+        //         }
+        //     }
+        //     if(flag){
+        //         pot = i;
+        //         break;
+        //     }
+        // }
+        // // Check for the pot candiate
+    //     for(int i{0}; i < n; ++i)
+    //     {
+    //         if(i != pot)
+    //         {
+    //             if(M[i][pot] != 1 || M[pot][i] == 1 )
+    //             {
+    //                 return -1;
+    //             }
+    //         }
+    //     }
+    //     return pot;
+    // }
+    
+    
+    int celebrity(vector<vector<int> >& M, int n){
+        // Creating stack to check for the potential candidate
+        stack <int> st;
+        for(int i{0}; i < n; ++i){
+            st.push(i);
+        }
+        while(st.size() >= 2)
         {
-            bool flag = true;
-            for(int j{0}; j < n; ++j)
-            {
-                if(M[i][j] != 0)
-                {
-                    flag = false;
-                    break;
-                }
+            // check consecutively wheter they know each other or not
+            int i = st.top();   st.pop();
+            int j = st.top();   st.pop();
+            // if i -> j push(j)
+            if(M[i][j] == 1){
+                st.push(j);
             }
-            if(flag){
-                pot = i;
-                break;
+            // else j -> i push(i) as if somebody know other then he can't be celebrity as per the given condition
+            else{
+                st.push(i);
             }
         }
-        // Check for the pot candiate
+        int pot = st.top();
+        st.pop();
+        // Check for the potential Candidate
         for(int i{0}; i < n; ++i)
         {
             if(i != pot)
             {
-                if(M[i][pot] != 1)
+                if(M[i][pot] == 0 || M[pot][i] == 1)
                 {
                     return -1;
                 }
@@ -44,6 +81,7 @@ class Solution
         }
         return pot;
     }
+   
 };
 
 //{ Driver Code Starts.
