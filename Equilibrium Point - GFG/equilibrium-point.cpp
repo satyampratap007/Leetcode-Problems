e6d19e2da7a3d4514f26795c69f199033d93dcdd
Satyam Pratap <satyampratap007@gmail.com>
2023-09-23 15:01:45 +0530

    Update equilibrium-point.cpp

--- Equilibrium Point - GFG/equilibrium-point.cpp
+++ Equilibrium Point - GFG/equilibrium-point.cpp
@@ -14,8 +14,9 @@ class Solution{
         int i = 0, j = n-1;
         while(i < j)
         {
-            if(s_i == s_j)
-            {
+            if(s_i == s_j) 
+            {              // take the case of 1 3 5 7 6 2 2 here s_i become equal to s_j at index 1 and 5 respectively, 
+                           // but that won't be my desired o/p so we will make it 0
                 s_i = 0;
                 s_j = 0;
             }
@@ -29,7 +30,7 @@ class Solution{
                 j--;
             }
         }
-        return (s_i == s_j) ? i+1 : -1;
+        return (s_i == s_j) ? i+1 : -1;  // when condition become true then i will reach to the equilibirium point 
     }
 };
 
