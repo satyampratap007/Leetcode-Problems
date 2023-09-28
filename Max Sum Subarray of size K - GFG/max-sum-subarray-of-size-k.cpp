59bd2fa6fac067b3537c70830c0e8b129190b289
Satyam Pratap <satyampratap007@gmail.com>
2023-09-28 16:14:45 +0530

    Added solution - LeetHub

---
+++ Max Sum Subarray of size K - GFG/max-sum-subarray-of-size-k.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    long maximumSumSubarray(int K, vector<int> &Arr , int N){
+        long maxi = INT_MIN,sum = Arr[0];
+        int i{0}, j{0};
+        while(j < N)
+        {
+            if(j-i+1 < K){
+                j++;
+                sum += Arr[j];
+            }
+            else{
+                maxi = max(sum, maxi);
+                sum -= Arr[i];
+                i++;
+            }
+        }
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N,K;
+        cin >> N >> K;;
+        vector<int>Arr;
+        for(int i=0;i<N;++i){
+            int x;
+            cin>>x;
+            Arr.push_back(x);
+        }
+        Solution ob;
+        cout << ob.maximumSumSubarray(K,Arr,N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
