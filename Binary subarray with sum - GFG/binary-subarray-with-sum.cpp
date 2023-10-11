39d3aac44ce58a6abf6d916479ee25e8e2f9cb37
Satyam Pratap <satyampratap007@gmail.com>
2023-10-12 01:57:54 +0530

    Added solution - LeetHub

--- Binary subarray with sum - GFG/binary-subarray-with-sum.cpp
+++ Binary subarray with sum - GFG/binary-subarray-with-sum.cpp
@@ -6,19 +6,41 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
   public:
+//   Sliding Window
+    // int numberOfSubarrays(vector<int>& arr, int N, int t){
+    //     return helper(arr,N,t) - helper(arr,N,t-1);
+    // }
+    // int helper(vector<int>& arr, int N, int t){
+    //     int i{0}, count{0};
+    //     for(int j{0}; j < N; ++j)
+    //     {
+    //         t -= arr[j];
+    //         while(t < 0)
+    //             t += arr[i++];
+    //         count += (j-i+1);
+    //     }
+    //     return count;
+    // }
+    
+    
+    
+    // Prefix sum and map
+    
+    
     int numberOfSubarrays(vector<int>& arr, int N, int t){
-        return helper(arr,N,t) - helper(arr,N,t-1);
-    }
-    int helper(vector<int>& arr, int N, int t){
-        int i{0}, count{0};
-        for(int j{0}; j < N; ++j)
+        int p_sum = 0;
+        unordered_map <int, int> mp({{0,1}});
+        int count = 0;
+        for(int i{0};i < N; ++i)
         {
-            t -= arr[j];
-            while(t < 0)
-                t += arr[i++];
-            count += (j-i+1);
+            p_sum += arr[i];
+            count += mp[p_sum - t]; // if found
+            mp[p_sum]++;
         }
         return count;
+        
+    
+        
     }
 };
 
